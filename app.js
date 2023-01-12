8a17777be4984cf5b67d46dbfbee5798ee2d621f
José Alexandre Ferreira <8140386@estg.ipp.pt>
2023-01-12 21:09:20 +0000

    about workingOn

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ const carousel = document.querySelector(".carousel");
 const arrowIcons = document.querySelectorAll(".wrapper i");
 const firstImage = document.querySelectorAll(".wrapper img")[0];
 
-let firstImgWidth = firstImage.clientWidth
+let firstImgWidth = firstImage.clientWidth;
 
 const showHideIcon = () => {
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-    if(carousel.scrollLeft == 0){
-        arrowIcons[0].style.display = "none";
-    }else if(carousel.scrollLeft == scrollWidth){
-        arrowIcons[1].style.display = "none";
-    }else{
-        arrowIcons[0].style.display = "block";
-        arrowIcons[1].style.display = "block";
-    }
-}
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  if (carousel.scrollLeft == 0) {
+    arrowIcons[0].style.display = "none";
+  } else if (carousel.scrollLeft == scrollWidth) {
+    arrowIcons[1].style.display = "none";
+  } else {
+    arrowIcons[0].style.display = "block";
+    arrowIcons[1].style.display = "block";
+  }
+};
 
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () =>{
-        if(icon.id == "leftArrow"){
-            carousel.scrollLeft -= firstImgWidth;
-            showHideIcon();
-        }else{
-            carousel.scrollLeft += firstImgWidth;
-            showHideIcon();
-        }
-        setTimeout(() => showHideIcon(), 60);
-    });
-})
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    if (icon.id == "leftArrow") {
+      carousel.scrollLeft -= firstImgWidth;
+      showHideIcon();
+    } else {
+      carousel.scrollLeft += firstImgWidth;
+      showHideIcon();
+    }
+    setTimeout(() => showHideIcon(), 60);
+  });
+});
